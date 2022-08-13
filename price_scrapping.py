daee1ac2824ce903387caaaf5195857f8bf6cf01
Lerato_vicky <lerato_vicky@localhost.localdomain>
2022-08-13 15:01:59 +0200

    "added csv feature, the information can now be transfared to csv

--- price_scrapping.py
+++ price_scrapping.py
@@ -9,15 +9,14 @@ soup = BeautifulSoup(req.text,'html.parser')
 results = soup.find_all('div',{'class':'product-tile'})
 
 
-# with open('price list.csv','w',encoding='utf8',newline='') as f:
-#     thewriter = writer(f)
-#     header = ["product name, prices"]
-#     thewriter.writerow(header)
+with open('price list.csv','w',encoding='utf8',newline='') as f:
+    thewriter = writer(f)
+    header = ["product name, prices"]
+    thewriter.writerow(header)
 
-for item in results:
-    products = item.find('div',{'class':'product-name'}).text.replace('\n',"")
-    price = item.find('span',{'class':'product-sales-price'}).text.replace('\n',"")
-     
-        # details = [products,price]
-        # thewriter.writerow(details)
-    print(products,float(price))
+    for item in results:
+        products = item.find('div',{'class':'product-name'}).text.replace('\n',"")
+        price = item.find('span',{'class':'product-sales-price'}).text.replace('\n',"")
+        
+        details = [products,price]
+        thewriter.writerow(details)
